387c7adcdf6f5b5ebbb8e8f758e52e74f40e3908
Aishika Vishwakarma <aishikavishwakarma@gmail.com>
2024-02-23 23:21:31 +0530

    Added Palindrome Linked List

---
+++ Palindrome Linked List.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+
+        res = []
+
+        while head:
+
+            res.append(head.val)
+
+            head = head.next
+
+        return True if res == res[::-1] else False
+        
