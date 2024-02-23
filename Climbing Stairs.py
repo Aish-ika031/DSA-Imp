ab3cd5e55ae559874f9a079e15dd4a1e895284c7
Aishika Vishwakarma <aishikavishwakarma@gmail.com>
2024-02-23 23:30:13 +0530

    Added Climbing Stairs

---
+++ Climbing Stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        dp = [0]*(45+1)
+
+        dp[1] , dp[2] = 1 , 2
+
+        for i in range(3 , 46):
+
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
+        
