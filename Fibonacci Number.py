05ea2c0ab5a884525380de436c8af91b3354e77c
Aishika Vishwakarma <aishikavishwakarma@gmail.com>
2024-03-12 22:22:44 +0530

    Added Fibonacci Number

---
+++ Fibonacci Number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        
+        def fibo(n):
+
+            if n == 0:
+
+                return 0
+
+            if n == 1:
+
+                return 1
+
+            return fibo(n-1) + fibo(n-2)
+
+        return fibo(n)
