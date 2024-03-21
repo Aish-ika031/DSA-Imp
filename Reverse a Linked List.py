5c878da81fa4028f03f0ad792e7438860c71b428
Aishika Vishwakarma <aishikavishwakarma@gmail.com>
2024-03-21 16:18:13 +0530

    Added Reverse Linked List

--- Reverse a Linked List.py
+++ Reverse a Linked List.py
@@ -7,13 +7,17 @@ class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
         current=head
+
         prev=None
         
         while current is not None:
             
             node=current.next
+
             current.next=prev
+
             prev=current
+            
             current=node
             
         head=prev
