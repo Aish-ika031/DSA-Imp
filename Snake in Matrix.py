72a3f9180f0914d982352900a09310723a2e213d
Aishika Vishwakarma <aishikavishwakarma@gmail.com>
2024-08-14 22:34:37 +0530

    Added Snake in Matrix

---
+++ Snake in Matrix.py
@@ -0,0 +1,24 @@
+class Solution:
+    def finalPositionOfSnake(self, n: int, commands: List[str]) -> int:
+
+        c = 0
+        
+        for i in commands:
+
+            if i == "DOWN":
+
+                C += N
+
+            elif i == "UP":
+
+                c -= n
+
+            elif i == "RIGHT":
+
+                c += 1
+
+            elif i == "LEFT":
+
+                c -= 1
+
+    return c
