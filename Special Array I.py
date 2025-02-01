43ba8f1638fb18f5349a5c7b426b8ea87bb23fff
Aishika Vishwakarma <100345686+Aish-ika031@users.noreply.github.com>
2025-02-01 14:50:09 +0530

    Added Special Array I

---
+++ Special Array I.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        
+        for i in range(len(nums)-1):
+
+            if nums[i] % 2 == nums[i+1] % 2:
+
+                return False
+
+        return True
