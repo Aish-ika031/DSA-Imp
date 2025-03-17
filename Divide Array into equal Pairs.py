f89a577acbba0691e1ad2f305da9f385404c4ef9
Aishika Vishwakarma <100345686+Aish-ika031@users.noreply.github.com>
2025-03-17 14:29:14 +0530

    Added Divide Array into equal Pairs

---
+++ Divide Array into equal Pairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        
+        cnt = Counter(nums)
+
+        for i in cnt:
+
+            if cnt[i] % 2:
+
+                return False
+
+        return True
